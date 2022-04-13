3b8ab13a8cb0bb27a3b56a65997516df6e8ceaa2
abigeater <abigeater@163.com>
2022-04-13 16:14:07 +0800

    add 支持在线人数

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -1,12 +1,6 @@
 import io from 'socket.io-client'
-const socket = io('ws://127.0.0.1:9502/',  { transports: ["websocket"] })
+const socket = io( process.env.socketUrl + '/',  { transports: ["websocket"] })
 socket.on('connect', data => {
-    socket.emit('event', 'hello, hyperf', console.log);
-    socket.emit('join-room', 'room1', console.log);
-    setInterval(function () {
-        socket.emit('say', '{"room":"room1", "message":"Hello Hyperf."}');
-    }, 1000);
+    socket.emit('join-room', 'main');
 });
-socket.on('event', console.log);
-console.log(112313)
 export default socket
